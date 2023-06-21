3cffc8edda810066939238997b00b92570825ff0
arionrefat <arionrefat@gmail.com>
2023-06-21 23:09:07 +0600

    added vercel analyitics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), mdx(), image()],
+  integrations: [tailwind(), mdx(), image()],
+  output: "server",
+  adapter: vercel()
 });
