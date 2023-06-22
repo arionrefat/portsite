99d144eee7426da3410b77a1ce39d53343d2454a
arionrefat <arionrefat@gmail.com>
2023-06-22 15:31:15 +0600

    added framer motion and react intgration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-
 import vercel from '@astrojs/vercel/serverless';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), mdx(), image()],
-	output: 'server',
-	adapter: vercel({
-		analytics: true,
-	}),
+  integrations: [tailwind(), mdx(), image(), react()],
+  output: 'server',
+  adapter: vercel({
+    analytics: true
+  })
 });
